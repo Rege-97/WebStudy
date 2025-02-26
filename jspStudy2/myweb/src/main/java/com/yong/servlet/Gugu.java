807c7c1daf2ad174f76d8aa62861695efdfe9b72
Rege-97 <cogus1027@gmail.com>
2025-02-26 15:15:37 +0900

    🧑‍💻 Study: 서블릿 구구단 실습

--- jspStudy2/myweb/src/main/java/com/yong/servlet/Gugu.java
+++ jspStudy2/myweb/src/main/java/com/yong/servlet/Gugu.java
@@ -2,24 +2,20 @@ package com.yong.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlert extends HttpServlet {
-
+public class Gugu extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		userProcess(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		userProcess(req, resp);
 	}
 
@@ -29,21 +25,22 @@ public class HelloServlert extends HttpServlet {
 
 		pw.println("<html>");
 		pw.println("<head><title>");
-		pw.println("이것은 서블릿");
+		pw.println("구구단");
 		pw.println("</title></head>");
 		pw.println("<body>");
-		pw.println("<h1>이것은 servlet입니다</h1>");
+		pw.println("<h1>구구단</h1>");
+		pw.println("<table border='1' width='650' cellspacing='0'>");
+		for (int i = 1; i <= 9; i++) {
+			pw.println("<tr>");
+			for (int j = 2; j <= 9; j++) {
+				pw.println("<td>");
+				pw.println(j+" * "+i+" = "+(i*j));
+				pw.println("</td>");
+			}
+			pw.println("</tr>");
+		}
+		pw.println("</table>");
 		pw.println("</body></html>");
-		
-		Calendar now=Calendar.getInstance();
-	      int y=now.get(Calendar.YEAR);
-	      int m=now.get(Calendar.MONTH)+1;
-	      int d=now.get(Calendar.DATE);
-	      
-	      pw.println("<h1>"+y+" 년 "+m+"월 "+d+"일<h1>");
-	      
-	      pw.println("</body></html>");
-	      pw.close();
-
 	}
+
 }
